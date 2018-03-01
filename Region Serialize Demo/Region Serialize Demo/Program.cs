34e8a5c00f20d22cf15784e1d92c0130d98b7584
myoder345 <myoder345@gmail.com>
2018-03-01 14:49:06 -0500

    Added my demo programs for real this time.

---
+++ Region Serialize Demo/Region Serialize Demo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Region_Serialize_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Layout FourSquare = new Layout
+            {
+                regions = new List<Region>
+                {
+                    new Region
+                    {
+                        Left = 0f,
+                        Top = 0f,
+                        Width = 0.5f,
+                        Height = 0.5f,
+                        Color = 0xff0000
+                    },
+
+                    new Region
+                    {
+                        Left = 0f,
+                        Top = 0.5f,
+                        Width = 0.5f,
+                        Height = 0.5f,
+                        Color = 0x0000ff
+                    },
+
+                    new Region
+                    {
+                        Left = 0.5f,
+                        Top = 0f,
+                        Width = 0.5f,
+                        Height = 0.5f,
+                        Color = 0x00ff00
+                    },
+
+                    new Region
+                    {
+                        Left = 0.5f,
+                        Top = 0.5f,
+                        Width = 0.5f,
+                        Height = 0.5f,
+                        Color = 0xffff00
+                    },
+                }
+            };
+            string json = JsonConvert.SerializeObject(FourSquare, Formatting.Indented);
+
+            System.IO.File.WriteAllText("FourSquare.json", json);
+        }
+    }
+}
---
+++ Region_Deserialize_Demo/Region_Deserialize_Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Region_Deserialize_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Layout layout = JsonConvert.DeserializeObject<Layout>(System.IO.File.ReadAllText("FourSquare.json"));
+
+            int width;
+            int height;
+
+            Console.Out.Write("Please enter screen width: ");
+            while(!Int32.TryParse(Console.ReadLine(), out width))
+            {
+                Console.Out.Write("Width must be an integer\nPlease enter screen width: ");
+            }
+
+            Console.Out.Write("Please enter screen height: ");
+            while (!Int32.TryParse(Console.ReadLine(), out height))
+            {
+                Console.Out.Write("Height must be an integer\nPlease enter screen width: ");
+            }
+
+            Console.Out.WriteLine("Resolution: " + width + "x" + height);
+
+            int i = 0;
+            foreach(Region r in layout.regions)
+            {
+                Console.Out.WriteLine("Region " + i + " spans from (" + r.Left * width + ", " + r.Top * height + ") to (" + ((r.Left + r.Width) * width - 1) + ", " + ((r.Top + r.Height) * height - 1) + ")");
+                ++i;
+            }
+
+            Console.Out.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
