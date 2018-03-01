f6e5f1d04a7afd2a8d3bf61b36e5d1f15f2c3b7c
myoder345 <myoder345@gmail.com>
2018-03-01 16:18:26 -0500

    Created base project, imported JSON toolkit and preexisting classes.

---
+++ SnapPeaApp/SnapPeaApp/Layout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapPeaApp
+{
+    class Layout
+    {
+        public List<Region> regions { get; set; }
+
+        public void AddRegion(Region r)
+        {
+            regions.Add(r);
+        }
+    }
+}
---
+++ SnapPeaApp/SnapPeaApp/Region.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnapPeaApp
+{
+    class Region
+    {
+        public float Left { get; set; }
+        public float Top { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public int Color { get; set; }
+    }
+}
