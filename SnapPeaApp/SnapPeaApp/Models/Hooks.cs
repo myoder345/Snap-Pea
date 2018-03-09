4c3695cb0b5942c7251a486e50f72e3f44f31b59
sadeslandes <sadeslandes@gmail.com>
2018-03-08 23:08:00 -0500

    Add hooks.cs to models

---
+++ SnapPeaApp/SnapPeaApp/Models/Hooks.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SplitScreenWindows
+{
+    class Hooks
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        #region Dll_Imports
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(ref Win32Point pt);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(HandleRef hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax,
+            IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess,
+            uint idThread, uint dwFlags);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWinEvent(IntPtr hWinEventHook);
+
+        const uint EVENT_SYSTEM_MOVESIZEEND = 0x000B;
+        const uint WINEVENT_OUTOFCONTEXT = 0;
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, object sender, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+        #endregion
+
+        IntPtr hhook;
+        WinEventDelegate procDelegate;
+
+        public Hooks()
+        {
+            procDelegate = new WinEventDelegate(WinEventProc);
+            hhook = SetWinEventHook(EVENT_SYSTEM_MOVESIZEEND, EVENT_SYSTEM_MOVESIZEEND, IntPtr.Zero,
+                   procDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+        }
+
+        Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+        public string CursorPos
+        {
+            get { return GetMousePosition().ToString(); }
+        }
+
+        void WinEventProc(IntPtr hWinEventHook, uint eventType,
+            IntPtr hwnd, object sender, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            int capacity = GetWindowTextLength(new HandleRef(sender, hwnd)) + 1;
+            StringBuilder stringBuilder = new StringBuilder(capacity);
+            GetWindowText(new HandleRef(sender, hwnd), stringBuilder, stringBuilder.Capacity);
+            Console.WriteLine($"Window {stringBuilder.ToString()} Moved, {CursorPos}");
+        }
+
+        public void Unhook()
+        {
+            UnhookWinEvent(hhook);
+        }
+    }
+}
