9bd0a98912bd6680af74d6984ae12283dfaef928
sadeslandes <sadeslandes@gmail.com>
2018-03-09 02:41:45 -0500

    Add viewmodel base class and relay command implementation

---
+++ SnapPeaApp/SnapPeaApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SnapPeaApp
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+		public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberNameAttribute] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
