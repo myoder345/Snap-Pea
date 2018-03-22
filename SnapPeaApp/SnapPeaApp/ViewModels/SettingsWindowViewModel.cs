5b9d6af2f8583c7f1859fe0a9bc5e9431857cd5f
lucasnot <notarl@rpi.edu>
2018-03-22 19:33:11 -0400

    Settings windows and related
    Added setting screen layout and whatnot

---
+++ SnapPeaApp/SnapPeaApp/ViewModels/SettingsWindowViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace SnapPeaApp.ViewModels
+{
+    class SettingsWindowViewModel : ViewModelBase
+    {
+        String layoutfolderpath;
+        public string LayoutFolderPath
+        {
+            get
+            {
+                return defaultlayoutpath;
+            }
+
+            set
+            {
+                SetProperty(ref layoutfolderpath, value);
+            }
+        }
+
+        public ICommand browsefolderpathcommand
+        {
+            get
+            {
+                return new RelayCommand(o => browsefolderpath());
+            }
+        }
+
+        private void browsefolderpath()
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.InitialDirectory = "c:\\";
+
+            var results = filedialog.ShowDialog();
+            if (results == DialogResult.OK)
+            {
+                LayoutFolderPath = filedialog.FileName;
+            }
+        }
+
+        String defaultlayoutpath;
+        public string DefaultLayoutPath
+        {
+            get
+            {
+                return defaultlayoutpath;
+            }
+
+            set
+            {
+                SetProperty(ref defaultlayoutpath, value);
+            }
+        }
+
+        public ICommand browselayoutcommand
+        {
+            get
+            {
+                return new RelayCommand(o => browsefolderpathtoo());
+            }
+        }
+
+        private void browsefolderpathtoo()
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            filedialog.InitialDirectory = "c:\\";
+
+            var results = filedialog.ShowDialog();
+            if(results == DialogResult.OK)
+            {
+                DefaultLayoutPath = filedialog.FileName;
+            }
+        }
+
+    }
+}
